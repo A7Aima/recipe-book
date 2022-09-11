7e59327fbe85d2f90957dca6c699dd709c055103
A7Aima <aimaabhilash@gmail.com>
2022-09-11 18:26:49 +0530

    delete form Array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -73,6 +73,9 @@ export class RecipeEditComponent implements OnInit {
       'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
     }));
   }
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 
   onCancel() {
     this.router.navigate(['../'], { relativeTo: this.route });
